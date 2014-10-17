062f02020326fc9908a66c26bdf09f83412dbf04
George <geocolon@gmail.com>
2014-10-17 00:46:39 -0400

    Change some things

--- calculator-box/js/calculator_box.js
+++ calculator-box/js/calculator_box.js
@@ -7,21 +7,44 @@ $(document).ready(function(){
 	$("#a10").click(function(){
 		//add 10 to the total
 		total = total + 10; // total += 10;
-	$("#a20").click(function(){
-		//add 10 to the total
-		total = total + 20; // total += 20;
-		
-
 		//update #out with the new total using the $("selector").html() 
 		$("#out").html(total);
+		$("#out").css("background-color", "pink");
+		$("#audio-1").trigger("play");
+		 	});
+		 	
+	$("#a20").click(function(){
+		total = total + 20;
+		$("#out").html(total);
+		$("#out").css("background-color", "0");
+		$("#audio-2").trigger("play");
 	});
-	$("#a10").html(total);
+	$("#a30").click(function(){
+		total = total + 30;
+		$("#out").html(total);
+		$("#audio-3").trigger("play");
 	});
-
+	$("#n10").click(function(){
+		total = total - 10;
+		$("#out").html(total);
+		$("#audio-4").trigger("play");
+	});
+	$("#n20").click(function(){
+		total = total - 20;
+		$("#out").html(total);
+		$("#audio-5").trigger("play");
+	});
+	$("#n30").click(function(){
+		total = total - 30;
+		$("#out").html(total);
+	$("#audio-6").trigger("play");	
+	});
 	//do this for a20, a30, n10, etc.
+	
 
 	$("#red").click(function(){ 
 		$("#out").css("background-color", "red");
+		$("#audio-7").trigger("play");	
 	});
 	
 	$("#blue").click(function(){ 
