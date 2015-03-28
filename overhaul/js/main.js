cc8667e873f45ee28476ff238eaebff1c2324565
Joe Frizzell <joefrizzell@gmail.com>
2015-03-28 14:29:41 -0400

    doit

--- overhaul/js/main.js
+++ overhaul/js/main.js
@@ -1,6 +1,14 @@
-$(document).ready(function() {
-	$(".fancybox").fancybox({
-		openEffect	: 'none',
-		closeEffect	: 'none'
-	});
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top-54
+        }, 500);
+        return false;
+      }
+    }
+  });
 });
