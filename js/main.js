f600c9243c48c7822f57431ab89a52b203ff6cee
Joe Frizzell <jfrizzell@penguinrandomhouse.com>
2015-04-03 14:04:25 -0400

    fixing scroll

--- js/main.js
+++ js/main.js
@@ -1,3 +1,17 @@
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top-54
+        }, 500);
+        return false;
+      }
+    }
+  });
+});
 $(document).ready(function() {
 	$(".fancybox-thumb").fancybox({
 		prevEffect	: 'none',
